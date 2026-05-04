472ddb4482bc654bd83a4fd4926953e2ff795828
ainihaa <ahqnrrrr@gmail.com>
2026-05-04 16:11:40 +0700

    Modul3

---
+++ biodata.php
@@ -0,0 +1,10 @@
+<?php
+$nim = '202457201009';
+$nama = 'Ainiha Qurrotul Aini';
+$alamat = 'Jl. Nakula Sadewa No.4';
+$nilai = 90;
+echo "NIM : ", $nim, "<br>";
+echo "Nama : ", $nama, "<br>";
+echo "Alamat : ", $alamat, "<br>";
+echo "Nilai : $nilai";
+?> 
